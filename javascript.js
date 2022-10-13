5d0549777a8330fc5dbb7f56e866d5e2b0ca9dd1
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-10-12 20:42:42 -0600

    Added test forEach function to retrieve data

--- javascript.js
+++ javascript.js
@@ -13,14 +13,12 @@ let library = [];
 console.log(library)
 //
 
-function Book (title, author, pages) {
+function Book (title, author, pages, completed) {
   this.title = title
   this.author = author
   this.pages = pages
-  // this.read = read (for checkbox)
-}
-
-let form = document.getElementById('bookForm'); 
+  this.completed = completed
+} 
 
 function addBookToLibrary(event) {
   event.preventDefault();
@@ -28,13 +26,37 @@ function addBookToLibrary(event) {
   let title = document.getElementById('title').value
   let author = document.getElementById('author').value
   let pages = document.getElementById('pages').value
+  let completed = document.getElementById('completed').checked
   
-  let newBook = new Book(title, author, pages)
+  let newBook = new Book(title, author, pages, completed)
   
   library.push(newBook)
+
   //
   console.log(newBook)
   console.log(library)
   //
+
+  //
+  addLibraryToShelf(library)
+  //
 }
+
+const form = document.getElementById('bookForm');
 form.addEventListener('submit', addBookToLibrary)
+
+function addLibraryToShelf (array) {
+  const bookShelf = document.querySelector('.book-shelf')
+  const addBook = document.createElement('div')
+  const addBookInfo = document.createElement('p')
+
+  array.forEach(function (bookElement) {
+    console.log(bookElement.title)
+    console.log(bookElement.author)
+    console.log(bookElement.pages)
+    console.log(bookElement.completed)
+    })
+
+    addBook.classList.add('book')
+    bookShelf.appendChild(addBook)
+  }
